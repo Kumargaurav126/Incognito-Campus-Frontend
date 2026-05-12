06b3b15600c7c452fd3ad05111550492ad133812
Kumargaurav126 <contact.kumargaurav12@gmail.com>
2026-05-12 16:31:42 +0530

    done

--- src/config.js
+++ src/config.js
@@ -1,4 +1,3 @@
 const API_URL = import.meta.env.VITE_API_URL || "http://192.168.1.4:8080/api";
 export const SUMMARISE_URL = import.meta.env.VITE_SUMMARISE_URL || "http://192.168.1.4:8081/api";
-export const WS_URL = import.meta.env.VITE_WS_URL || "http://192.168.1.4:8080";
 export default API_URL;
